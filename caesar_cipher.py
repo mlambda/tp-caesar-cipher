9d3c54c221e997bc8f13444cf19f85effc150af6
m09 <142691+m09@users.noreply.github.com>
2021-09-28 00:29:51 +0200

    Sync
    Signed-off-by: m09 <142691+m09@users.noreply.github.com>

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,12 +1,15 @@
 from string import ascii_lowercase, ascii_uppercase
 from typing import Dict, Optional
 
+import deal
+
 
 def make_table(characters: str, key: int) -> Dict[int, Optional[int]]:
     key %= 26
     return str.maketrans(characters, characters[key:] + characters[:key])
 
 
+@deal.pre(lambda text, key: 0 <= key < 26)
 def rotate(text: str, key: int) -> str:
     table = {**make_table(ascii_lowercase, key), **make_table(ascii_uppercase, key)}
     return text.translate(table)
