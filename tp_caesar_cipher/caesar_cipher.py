4047ec6effa350afb07a0596fd3ea5f845b75e67
m09 <142691+m09@users.noreply.github.com>
2023-05-26 15:37:19 +0200

    Add tests
    Signed-off-by: m09 <142691+m09@users.noreply.github.com>

--- tp_caesar_cipher/caesar_cipher.py
+++ tp_caesar_cipher/caesar_cipher.py
@@ -1,11 +1,13 @@
 from string import ascii_lowercase, ascii_uppercase
 
+import deal
+
 
 def make_table(characters: str, key: int) -> dict[int, int]:
-    key %= 26
     return str.maketrans(characters, characters[key:] + characters[:key])
 
 
+@deal.pre(lambda text, key: 0 <= key < 26)
 def rotate(text: str, key: int) -> str:
     table = {**make_table(ascii_lowercase, key), **make_table(ascii_uppercase, key)}
     return text.translate(table)
